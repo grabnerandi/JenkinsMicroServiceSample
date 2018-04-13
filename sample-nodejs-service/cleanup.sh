c4d5042180ce721ef3b55508fadbf5ec0771b790
Andreas Grabner <grabnerandi@gmx.at>
2018-04-14 00:33:58 +0200

    better cleanup

--- sample-nodejs-service/cleanup.sh
+++ sample-nodejs-service/cleanup.sh
@@ -1,3 +1,4 @@
 IMAGENAME=$1
 docker images | grep ${IMAGENAME} | awk '{print $1 }' | xargs -I {} docker stop {}
+docker rm $(docker ps -a -q)
 docker ps -a | awk '{ print $1,$2 }' | grep ${IMAGENAME} | awk '{print $1 }' | xargs -I {} docker rm {}
